c19260145ce08a954bbbb54039f3fb2bfa0734f5
Shan052 <ahmad.shan2488@gmail.com>
2026-02-13 14:20:24 +0530

    second commit

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from grading_model import final_score
 
+from fastapi import Header, HTTPException
+
 app = FastAPI()
 
 class AnswerRequest(BaseModel):
     sample_answer: str
     student_answer: str
 
+
+
+
+API_KEY = "your_secret_key"
+
 @app.post("/grade")
-def grade_answer(data: AnswerRequest):
-    score = final_score(data.sample_answer, data.student_answer)
+def grade_answer(data: AnswerRequest, x_api_key: str = Header(None)):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=403, detail="Unauthorized")
+
+    score = final_score(data.correct_answer, data.student_answer)
     return {"score": score}
+
